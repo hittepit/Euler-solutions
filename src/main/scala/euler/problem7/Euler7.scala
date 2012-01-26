470b25eb0b6210fbb4df3064ad9a2565f3d878ba
fabrice.claes <fabrice.claes@ucm.be>
2012-01-26 16:50:29 +0100

    Solution problem 7

---
+++ src/main/scala/euler/problem7/Euler7.scala
@@ -0,0 +1,39 @@
+package euler{
+  package problem7{
+    
+	import problem3.Euler3
+	import utils.Timer
+	
+	/**
+	 * Solution 1 more functionnal but much more slower. Needs a max number (105000) empiracally determined
+	 */
+	object Euler7 extends Timer{
+		def main(args: Array[String]) {
+	//		println(solve1(10001))
+			println(solve2(10001))
+		}
+		
+	//	def solve1(nth:Int):Long = time("solution1"){() =>
+	//	  val r = (2L to 105000L).toList
+	//	  findPrime(r,1,nth)
+	//	}
+	//	
+	//	def findPrime(candidates:List[Long], index:Int, nth:Int):Long = candidates match {
+	//	  case n::l => if(index == nth) n else findPrime(l.filter{_%n!=0}, index+1, nth)
+	//	  case _ => 0L
+	//	}
+		
+		def solve2(nth:Int):Long = time("solution2"){() =>
+		  var index = 0
+		  var start:Long = 1L
+		  while(index<nth){
+			  start+=1
+			  if(Euler3.isPrimeV1(start)){
+			    index +=1
+			  }
+		  }
+		  start
+		}
+	}
+  }
+}
