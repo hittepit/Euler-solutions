5519d772524098f2292f3071f9002a8fd4fb9ba7
fabrice.claes <fabrice.claes@ucm.be>
2012-02-01 14:21:43 +0100

    Solution problem 35

---
+++ src/main/scala/euler/problem35/Euler35.scala
@@ -0,0 +1,27 @@
+package euler.problem35
+
+import euler.utils.Primes._
+import scala.collection._
+
+object Euler35 {
+	def main(args: Array[String]) {
+		val max = 1000000
+		
+		val circularPrimes = (Set[Int]()/:(2 to max))((primes:Set[Int],candidate) =>
+			if((true /: rotations(candidate.toString()))((isPrime:Boolean,s:String)=>isPrime && isPrimeV2(s.toInt)))
+			  primes+candidate
+			  else
+			    primes
+		  )
+		  
+		  println(circularPrimes.size)
+	}
+	
+	def rotations(s:String):mutable.Set[String] ={
+	  val rot = mutable.Set[String]()
+	  (0 to s.length()).foreach{(index:Int) =>
+	    rot+=s.drop(s.length-index)+s.dropRight(index)
+	  }
+	  rot
+	} 
+}
