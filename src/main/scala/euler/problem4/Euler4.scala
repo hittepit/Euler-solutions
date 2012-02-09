04e1d13ae1fd837c39b335ee218c6f4055be1a0d
fabrice.claes <fabrice.claes@ucm.be>
2012-02-09 11:17:24 +0100

    Refactoring Euler 4

--- src/main/scala/euler/problem4/Euler4.scala
+++ src/main/scala/euler/problem4/Euler4.scala
@@ -33,28 +33,16 @@ object Euler4 {
 
 class Euler4 extends Solution {
 	def execute = {
-		var solution=(0,0,0)
-		var alpha = 999
-		while(alpha>99){
-		  var beta = alpha
-		  while(beta>99){
+		var solution=0
+		for(alpha <- 999 to 100 by -1 ; beta <- alpha to 100 by -1){
 		    val result = alpha*beta
-		    if(isPalindrome(result) && result>solution._3) {
-		      solution = (alpha,beta,result)
+		    if(isPalindrome(result.toString) && result>solution) {
+		      solution = result
 		    }
-		    beta-=1
-		  }
-		  alpha-=1
 		}
 		
-		solution._3.toString()
+		solution.toString()
 	}
-	def isPalindrome(i:Int) = {
-	  val s=i.toString()
-	  val center = s.length()/2-1
-	  (0 to center).find((x:Int)=>s(x)!=s(s.length()-x-1)) match {
-	    case None => true
-	    case _ => false
-	  }
-	}
+	
+	def isPalindrome(s:String) = ! (0 to  s.length()/2-1).exists{(x:Int) => s(x) != s(s.length-x-1)}
 }
