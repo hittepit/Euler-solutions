989a5cd62f5023f0dfe156b146451c62736a3b5b
Fabrice <claesfab@skynet.be>
2012-02-09 21:11:53 +0100

    Solution problem 47. Way too slow !

--- src/main/scala/euler/problem47/Euler47.scala
+++ src/main/scala/euler/problem47/Euler47.scala
@@ -13,18 +13,30 @@ object Euler47 {
 class Euler47{
 	val consecutive = 4
 	def execute = {
-	  sequence((1L/:primes(2).take(consecutive))(_*_)).find{(n:Long) =>
-	    !(n+0 until n+consecutive).exists{(index:Long) =>countPrimeFactors(index)!=consecutive}
+	  var n =  2*3*5*7
+	  while(!hasXPrimeFactors(n,consecutive) || !hasXPrimeFactors(n+1,consecutive) || !hasXPrimeFactors(n+2,consecutive) || !hasXPrimeFactors(n+3,consecutive)){
+	    n+=1
 	  }
+	  n.toString
 	}
 	
-	def countPrimeFactors(n:Long) = {
-		(0L/:primes(2).takeWhile(_<n)){(total: Long, prime:Long) => 
-	    	if(n%prime==0) {
-	    		  total+1
-	    	}else{
-	    		total
-	    	}
-		}
+	def hasXPrimeFactors(number:Long,x:Int) = {
+	  if(isPrime(number))
+	    false
+	    else {
+		  var total = 0
+		  val primeGenerator = primes(2).iterator
+		  var n = primeGenerator.next
+		  var rest = number
+		  while(total<=x && n<=rest){
+		    if(rest%n == 0) total+=1
+		    while(rest%n==0){
+		      rest/=n
+		    }
+		    n = primeGenerator.next
+		  }
+		  if(total>x) println(number +" ->"+total)
+		  total==x
+	    }
 	}
 }
