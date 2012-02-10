f1a5452084abf3d52e254c74822a3cc1e2a1ff04
fabrice.claes <fabrice.claes@ucm.be>
2012-02-10 09:03:50 +0100

    A little gain in performance on euler 47, so little...

--- src/main/scala/euler/problem47/Euler47.scala
+++ src/main/scala/euler/problem47/Euler47.scala
@@ -1,7 +1,7 @@
 package euler.problem47
 
 import euler.utils.Math._
-
+import euler.utils.Timer
 import scala.math._
 
 object Euler47 {
@@ -10,14 +10,25 @@ object Euler47 {
 	}
 }
 
-class Euler47{
+class Euler47 extends Timer{
 	val consecutive = 4
-	def execute = {
-	  var n =  2*3*5*7
-	  while(!hasXPrimeFactors(n,consecutive) || !hasXPrimeFactors(n+1,consecutive) || !hasXPrimeFactors(n+2,consecutive) || !hasXPrimeFactors(n+3,consecutive)){
-	    n+=1
-	  }
-	  n.toString
+	def execute = time("Euler47"){() =>
+	  var n =  (1L/:primes(2).take(consecutive))(_*_)
+	  var cons = 0
+	  do{
+		  while(hasXPrimeFactors(n,consecutive) && cons<consecutive){
+		    n+=1
+		    cons+=1
+		  }
+		  if(cons!=consecutive){
+		    n+=1
+		    cons=0
+		  }
+	  }while(cons!=consecutive)
+//	  while(!hasXPrimeFactors(n,consecutive) || !hasXPrimeFactors(n+1,consecutive) || !hasXPrimeFactors(n+2,consecutive) || !hasXPrimeFactors(n+3,consecutive)){
+//	    n+=1
+//	  }
+	  (n-cons).toString
 	}
 	
 	def hasXPrimeFactors(number:Long,x:Int) = {
@@ -35,7 +46,6 @@ class Euler47{
 		    }
 		    n = primeGenerator.next
 		  }
-		  if(total>x) println(number +" ->"+total)
 		  total==x
 	    }
 	}
